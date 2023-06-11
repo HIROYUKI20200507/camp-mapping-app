9620ae0e8befa5974ecc3a7f18de58a0f792f92f
ak-akimoto-peraichi <dokomademo1124@gmail.com>
2023-06-11 22:47:56 +0900

    create: gif画像を追加

--- pages/landing.vue
+++ pages/landing.vue
@@ -85,19 +85,46 @@ onUnmounted(() => {
     </section>
 
     <!-- 特徴セクション -->
-    <section class="py-10">
-      <h2 class="text-3xl font-bold mb-6 text-center">アプリの特徴</h2>
-      <div class="flex flex-wrap justify-center">
+    <section class="py-10 m-auto max-w-5xl">
+      <div class="text-center mb-6">
+        <h2 class="text-3xl font-bold text-center">アプリの特徴</h2>
+        <span class="inline-block border-b-4 border-cyan-400 w-20"></span>
+      </div>
+
+      <div class="flex flex-wrap justify-center text-center">
         <div class="w-full sm:w-1/3 p-4">
-          <h3 class="text-2xl font-bold mb-2">直感的な地図検索</h3>
-          <p>地図上で簡単にキャンプ場を見つけることができます。</p>
+          <h3 class="text-2xl font-bold">直感的な地図検索</h3>
+          <div class="w-2/3 m-auto">
+            <img
+              src="~/assets/images/landings/compass.gif"
+              alt="地図検索のイメージ"
+              class="mb-4"
+            />
+          </div>
+          <p>
+            都道府県から市区町村を選択すると付近のキャンプ場が一覧で閲覧することができます。
+          </p>
         </div>
         <div class="w-full sm:w-1/3 p-4">
           <h3 class="text-2xl font-bold mb-2">詳細情報</h3>
+          <div class="w-2/3 m-auto">
+            <img
+              src="~/assets/images/landings/mapping_car.gif"
+              alt="詳細情報のイメージ"
+              class="mb-4"
+            />
+          </div>
           <p>キャンプ場の詳細情報を確認して、最適な場所を見つけましょう。</p>
         </div>
         <div class="w-full sm:w-1/3 p-4">
           <h3 class="text-2xl font-bold mb-2">レスポンシブデザイン</h3>
+          <div class="w-2/3 m-auto">
+            <img
+              src="~/assets/images/landings/phone_S.gif"
+              alt="レスポンシブデザインのイメージ"
+              class="mb-4"
+            />
+          </div>
           <p>デバイスに関係なく、アプリを快適に利用できます。</p>
         </div>
       </div>
@@ -105,13 +132,15 @@ onUnmounted(() => {
 
     <!-- CTAセクション -->
     <section class="bg-blue-100 py-10">
-      <h2 class="text-3xl font-bold mb-6 text-center">
-        今すぐキャンプ場を検索しましょう！
-      </h2>
-      <div class="text-center">
-        <nuxt-link class="bg-blue-600 text-white px-4 py-2 rounded" to="/app"
-          >アプリを試す</nuxt-link
-        >
+      <div class="m-auto max-w-5xl">
+        <h2 class="text-3xl font-bold mb-6 text-center">
+          今すぐキャンプ場を検索しましょう！
+        </h2>
+        <div class="text-center">
+          <nuxt-link class="bg-blue-600 text-white px-4 py-2 rounded" to="/app"
+            >アプリを試す</nuxt-link
+          >
+        </div>
       </div>
     </section>
   </div>
