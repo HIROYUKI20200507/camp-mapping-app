d8835b26652e6e9c932527ab8a331ee074c57225
ak-akimoto-peraichi <dokomademo1124@gmail.com>
2023-06-20 10:53:45 +0900

    update: ssgに変更

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 import { resolve } from "path";
 export default defineNuxtConfig({
+  ssr: false,
   app: {
     head: {
       title: "誰でもカンタンに素早く検索できるアプリ｜QuickCamp〜クイキャン〜",
@@ -11,13 +12,7 @@ export default defineNuxtConfig({
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
       ],
-      link: [
-        {
-          rel: "icon",
-          type: "image/x-icon",
-          href: "/logo.png",
-        },
-      ],
+      link: [{ rel: "icon", type: "image/png", href: "/favicon.png" }],
     },
   },
   vite: {
