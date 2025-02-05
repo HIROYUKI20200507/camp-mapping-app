2de02a21eba37dc4c065c42cc07dba8799667954
hiroyuki.akimoto <dokomademo1124@gmail.com>
2025-02-05 23:17:11 +0900

    update: Tailwind CSSと依存関係のバージョンを更新し、設定を最適化

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,40 +1,32 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-import { resolve } from "path";
+import { resolve } from "node:path";
 export default defineNuxtConfig({
-  ssr: false,
   app: {
     head: {
       title: "誰でもカンタンに素早く検索できるアプリ｜QuickCamp〜クイキャン〜",
       htmlAttrs: {
         lang: "ja",
       },
-      meta: [
-        { charset: "utf-8" },
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
-      ],
+      meta: [{ charset: "utf-8" }, { name: "viewport", content: "width=device-width, initial-scale=1" }],
       link: [{ rel: "icon", type: "image/png", href: "/favicon.png" }],
     },
   },
+
   vite: {
     define: {
       "process.env.DEBUG": false,
     },
   },
-  css: [
-    "@/assets/css/tailwind.css",
-    "@fortawesome/fontawesome-svg-core/styles.css",
-  ],
+
+  css: ["@/assets/css/tailwind.css", "@fortawesome/fontawesome-svg-core/styles.css"],
+
   plugins: ["@/plugins/fontawesome.ts"],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
+
   alias: {
     "~/Components": resolve(__dirname, "./components"),
     "~/Assets": resolve(__dirname, "./assets"),
   },
+
   runtimeConfig: {
     googleGeoCodingApiKey: process.env.GOOGLE_API_GEO_CODING_KEY,
     resasApiKey: process.env.RESAS_API_KEY,
@@ -44,6 +36,7 @@ export default defineNuxtConfig({
       resasApiKey: process.env.RESAS_API_KEY,
     },
   },
+
   nitro: {
     preset: "firebase",
     devProxy: {
@@ -53,4 +46,6 @@ export default defineNuxtConfig({
       },
     },
   },
+
+  compatibilityDate: "2025-02-05",
 });
